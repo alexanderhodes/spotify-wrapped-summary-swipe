709604677e273cd9b209fe50ae5ead083f1c209d
alexanderhodes <alexander.hodes@live.com>
2022-12-15 08:48:13 +0100

    feat: added summary slider

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
-import { Summary } from "./src/components/Summary";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { Summary, SUMMARY_HEIGHT } from "./src/components/Summary";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Summary backgroundColor="green" textColor="white" />
+      <View style={{ height: SUMMARY_HEIGHT }}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <View style={[styles.summaryContainer]}>
+            <Summary backgroundColor="green" textColor="white" />
+          </View>
+          <View style={[styles.summaryContainer]}>
+            <Summary backgroundColor="red" textColor="white" />
+          </View>
+          <View style={[styles.summaryContainer]}>
+            <Summary backgroundColor="blue" textColor="white" />
+          </View>
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -13,7 +25,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  summaryScrollContainer: {
+    marginBottom: 12,
+  },
+  summaryContainer: {
+    paddingHorizontal: 8,
   },
 });
